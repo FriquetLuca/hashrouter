71d32d82017bc82e42d0144acb2f093ceaa396e0
Ikikaeru <39247240+Ikikaeru@users.noreply.github.com>
2023-02-01 17:12:44 +0100

    Checking 404 location

--- src/routing/skip404.js
+++ src/routing/skip404.js
@@ -1,9 +1,11 @@
 import { config } from "../config/config.js";
 const projectLength = config.projectName.length;
 const prefix = projectLength > 0 ? `/${config.projectName}/#` : '/#';
-const newLocation = `${prefix}${window.location.pathname.substr(projectLength + 1)}`;
+let newLocation = `${prefix}${window.location.pathname.substr(projectLength + 1)}`;
 if(window.location.search.length > 0) {
-    window.location.replace(`${newLocation}/${window.location.search}`);
-} else {
-    window.location.replace(newLocation);
+    newLocation = `${newLocation}/${window.location.search}`;
 }
+console.log(`Replace location: ${newLocation}`);
+setTimeout(() => {
+    window.location.replace(newLocation);
+}, 10000);
