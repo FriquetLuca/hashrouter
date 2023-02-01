6d09839d8336834b7e7e8487083bfdeeb46457a8
Ikikaeru <39247240+Ikikaeru@users.noreply.github.com>
2023-02-01 17:07:32 +0100

    Redirect params from 404

--- src/routing/skip404.js
+++ src/routing/skip404.js
@@ -1,4 +1,9 @@
 import { config } from "../config/config.js";
 const projectLength = config.projectName.length;
 const prefix = projectLength > 0 ? `/${config.projectName}/#` : '/#';
-window.location.replace(`${prefix}${window.location.pathname.substr(projectLength + 1)}`);
+const newLocation = `${prefix}${window.location.pathname.substr(projectLength + 1)}`;
+if(window.location.search.length > 0) {
+    window.location.replace(`${newLocation}/${window.location.search}`);
+} else {
+    window.location.replace(newLocation);
+}
