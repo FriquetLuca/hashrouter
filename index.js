85215a71766714ce879b55cc4e16be1e7b83715f
Ikikaeru <39247240+Ikikaeru@users.noreply.github.com>
2023-02-01 14:32:50 +0100

    Remove the last slash for path testing

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
+const projectName = "hashrouter";
 const content = document.getElementById('content');
 
 const routes = {
   '/hashrouter/': () => {
-    content.innerHTML = '<h1>Home</h1>';
+    content.innerHTML = '<h1>Home page</h1>';
   },
   '/home': () => {
-    content.innerHTML = '<h1>Home</h1>';
+    content.innerHTML = '<h1>Home page</h1>';
   },
   '/about': () => {
-    content.innerHTML = '<h1>About</h1>';
+    content.innerHTML = '<h1>About page</h1>';
   },
   '/contact': () => {
-    content.innerHTML = '<h1>Contact</h1>';
+    content.innerHTML = '<h1>Contact page</h1>';
   },
   '/404': () => {
     content.innerHTML = '<h1>404 Page Not Found</h1>';
   }
 };
+routes[`/${projectName}`] = routes['/home'];
 
 const handleNavigation = (path) => {
-  const route = routes[path] || routes['/404'];
+  const route = routes[(path.length > 1 && path[path.length - 1] === '/') ? path.substring(0, path.length - 1) : path] || routes['/404'];
   route();
 };
 
@@ -28,7 +30,7 @@ document.addEventListener('click', (event) => {
   if (target.tagName === 'A') {
     event.preventDefault();
     const href = target.getAttribute('href');
-    history.pushState({}, '', '/hashrouter' + href);
+    history.pushState({}, '', `/${projectName}${href}`);
     handleNavigation(href);
   }
 });
@@ -40,7 +42,7 @@ window.addEventListener('popstate', (event) => {
 const splittedURL = location.href.split("#");
 switch(splittedURL.length) {
   case 2:
-    history.pushState({}, '', '/hashrouter' + splittedURL[1]);
+    history.pushState({}, '', `/${projectName}${splittedURL[1]}`);
     handleNavigation(splittedURL[1]);
     break;
   default:
