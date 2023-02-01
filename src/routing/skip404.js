0260171805608e76225c1905b06f8704acaa4349
Ikikaeru <39247240+Ikikaeru@users.noreply.github.com>
2023-02-01 17:33:23 +0100

    Remove the 404 test

--- src/routing/skip404.js
+++ src/routing/skip404.js
@@ -3,9 +3,6 @@ const projectLength = config.projectName.length;
 const prefix = projectLength > 0 ? `/${config.projectName}/#` : '/#';
 let newLocation = `${prefix}${window.location.pathname.substr(projectLength + 1)}`;
 if(window.location.search.length > 0) {
-    newLocation = `${newLocation}/${window.location.search}`;
+    newLocation = `${newLocation}${window.location.search}`;
 }
-console.log(`Replace location: ${newLocation}`);
-setTimeout(() => {
-    window.location.replace(newLocation);
-}, 10000);
+window.location.replace(newLocation);
