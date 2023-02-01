714748ad0cb5405291ef115a272ff09a16ae9507
Ikikaeru <39247240+Ikikaeru@users.noreply.github.com>
2023-02-01 12:07:43 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hash Router
+
+> Just a basic way to achieve a hash router using only HTML and JavaScript.
---
+++ index.js
@@ -0,0 +1,37 @@
+const content = document.getElementById('content');
+
+const routes = {
+  '/home': () => {
+    content.innerHTML = '<h1>Home</h1>';
+  },
+  '/about': () => {
+    content.innerHTML = '<h1>About</h1>';
+  },
+  '/contact': () => {
+    content.innerHTML = '<h1>Contact</h1>';
+  },
+  '*': () => {
+    content.innerHTML = '<h1>404 Page Not Found</h1>';
+  }
+};
+
+const handleNavigation = (path) => {
+  const route = routes[path] || routes['/home'];
+  route();
+};
+
+document.addEventListener('click', (event) => {
+  const target = event.target;
+  if (target.tagName === 'A') {
+    event.preventDefault();
+    const href = target.getAttribute('href');
+    history.pushState({}, '', href);
+    handleNavigation(href);
+  }
+});
+
+window.addEventListener('popstate', (event) => {
+  handleNavigation(location.pathname);
+});
+
+handleNavigation(location.pathname);
